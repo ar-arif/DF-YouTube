7119bff0726f330fef1d9b9aa349f02d9907812d
zer0 <just.ar.arif@gmail.com>
2022-01-19 10:52:44 +0600

    cookie added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let watchBTN = document.querySelector("#watch");
 let yt = document.querySelector("#yt");
 let ytLink = document.querySelector("#inputURL");
+function loadLink() {
+  ytLink.value = document.cookie;
+  if (ytLink.value.length != 0) {
+    video();
+  }
+}
 
 watchBTN.addEventListener("click", video);
 ytLink.addEventListener("keyup", (event) => {
@@ -11,6 +17,7 @@ ytLink.addEventListener("keyup", (event) => {
 
 function video() {
   let url = ytLink.value.trim();
+  document.cookie = url;
   let id;
   if (is_url(url)) {
     if (url.includes("list=")) {
@@ -23,7 +30,7 @@ function video() {
       id = url.slice(17);
     }
     console.log(id);
-    yt.innerHTML = `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    yt.innerHTML = `<iframe width="100%" height="500" src="https://www.youtube.com/embed/${id}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   } else {
     let alert = document.querySelector("#alert");
     alert.classList.remove("d-none");
@@ -31,7 +38,8 @@ function video() {
 }
 
 function is_url(str) {
-  regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+  regexp =
+    /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
   if (regexp.test(str)) {
     return true;
   } else {
